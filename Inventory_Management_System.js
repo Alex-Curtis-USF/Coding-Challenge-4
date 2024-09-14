d6d47d49274da758b1345579cbdaf060dcbc4f35
Alexander Curtis <ajcurtis@usf.edu>
2024-09-14 17:35:29 -0400

    Initialize inventory with product objects

---
+++ Inventory_Management_System.js
@@ -0,0 +1,29 @@
+// Create an Inventory Array of Product Objects
+
+let inventory = [ {
+    "name": "Notebook",
+    "price": 3,
+    "quantity": 12,
+    "lowStockLevel": 10
+}, {
+    "name": "Paper",
+    "price": 5,
+    "quantity": 30,
+    "lowStockLevel": 40
+}, {
+    "name": "Pen",
+    "price": 2,
+    "quantity": 100,
+    "lowStockLevel": 50
+}, {
+    "name": "Pencil",
+    "price": 1,
+    "quantity": 75,
+    "lowStockLevel": 100
+}, {
+    "name": "Stapler",
+    "price": 10,
+    "quantity": 20,
+    "lowStockLevel": 10
+}
+]
