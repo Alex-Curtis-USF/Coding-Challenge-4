87328dfc8a11a3da56f958c78b8a55f6d1552436
Alexander Curtis <ajcurtis@usf.edu>
2024-09-15 00:02:58 -0400

    Create updateStock function

--- Inventory_Management_System.js
+++ Inventory_Management_System.js
@@ -1,30 +1,30 @@
 // Create an Inventory Array of Product Objects
 
 let inventory = [ {
-    "name": "Notebook",
-    "price": 3,
-    "quantity": 12,
-    "lowStockLevel": 10
+    name: 'Notebook',
+    price: 3,
+    quantity: 12,
+    lowStockLevel: 10
 }, {
-    "name": "Paper",
-    "price": 5,
-    "quantity": 30,
-    "lowStockLevel": 40
+    name: 'Paper',
+    price: 5,
+    quantity: 30,
+    lowStockLevel: 40
 }, {
-    "name": "Pen",
-    "price": 2,
-    "quantity": 100,
-    "lowStockLevel": 50
+    name: 'Pen',
+    price: 2,
+    quantity: 100,
+    lowStockLevel: 50
 }, {
-    "name": "Pencil",
-    "price": 1,
-    "quantity": 75,
-    "lowStockLevel": 100
+    name: 'Pencil',
+    price: 1,
+    quantity: 75,
+    lowStockLevel: 100
 }, {
-    "name": "Stapler",
-    "price": 10,
-    "quantity": 20,
-    "lowStockLevel": 10
+    name: 'Stapler',
+    price: 10,
+    quantity: 20,
+    lowStockLevel: 10
 }
 ]
 
@@ -39,3 +39,18 @@ function displayProductDetails(inventory) {
     console.log(`In Stock: ${IsItInStock}`);
 }
 
+//Create displayProductDetails function
+
+function updateStock(inventory, unitsSold) {
+    inventory.quantity -= unitsSold;
+    inventory.quantity = Math.max(inventory.quantity, 0);
+    if (inventory.quantity === 0) {
+        return `${product.name} is out of stock`
+    } 
+    else if (inventory.quantity <= inventory.lowStockLevel) {
+        return `${product.name} is low in stock. The current quantity is ${product.quantity}`
+    }
+    else {
+        return `${product.name} is in stock. The current quantity is ${product.quantity}`;
+    }
+}
